06cad4aa1f8e056638a817ca1d342cf7bfd3d971
Rob Dodson <robdodson@google.com>
2016-08-08 12:01:30 -0700

    add PSK template

---
+++ generators/templates/index.js
@@ -0,0 +1,19 @@
+'use strict';
+const yeoman = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require('path');
+
+module.exports = yeoman.Base.extend({
+  writing: function () {
+    this.sourceRoot(path.join(path.dirname(this.resolved), 'polymer-starter-kit'));
+    this.fs.copy([
+      this.templatePath(),
+      '!**/{sw-precache-config.js,service-worker.js}'
+    ], this.destinationPath());
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
