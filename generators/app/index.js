29db2f594fde2bda05dfa61e95338f0662e53968
Abdón Rodríguez Davila <a@abdonrd.com>
2016-08-09 18:17:10 +0200

    Add license and sort imports (#8)

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,23 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
 'use strict';
-const yeoman = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+
 const path = require('path');
 
+const chalk = require('chalk');
+const yeoman = require('yeoman-generator');
+const yosay = require('yosay');
+
 module.exports = yeoman.Base.extend({
-  writing: function () {
+  writing: function() {
     this.sourceRoot(path.join(path.dirname(this.resolved), 'polymer-starter-kit'));
     this.fs.copy([
       this.templatePath(),
@@ -18,7 +30,7 @@ module.exports = yeoman.Base.extend({
     ], this.destinationPath());
   },
 
-  install: function () {
+  install: function() {
     this.installDependencies();
   }
 });
