69fd5751d1eb8baf120502570ab8d17a21198e7f
Fred K. Schott <fkschott@gmail.com>
2017-02-20 19:11:22 -0800

    update polymer-build docs, use v0.8.1 (#60)

--- generators/app/gulpfile.js
+++ generators/app/gulpfile.js
@@ -34,30 +34,50 @@ function waitFor(stream) {
 
 function build() {
   return new Promise((resolve, reject) => { // eslint-disable-line no-unused-vars
+
+    // Lets create some inline code splitters in case you need them later in your build.
+    let sourcesStreamSplitter = new polymerBuild.HtmlSplitter();
+    let dependenciesStreamSplitter = new polymerBuild.HtmlSplitter();
+
     // Okay, so first thing we do is clear the build directory
     console.log(`Deleting ${buildDirectory} directory...`);
     del([buildDirectory])
       .then(() => {
-        // Okay, now let's get your source files
+
+        // Let's start by getting your source files. These are all the files
+        // in your `src/` directory, or those that match your polymer.json
+        // "sources"  property if you provided one.
         let sourcesStream = polymerProject.sources()
-          // Oh, well do you want to minify stuff? Go for it!
-          // Here's how splitHtml & gulpif work
-          .pipe(polymerProject.splitHtml())
-          // .pipe(gulpif(/\.js$/, uglify()))
-          // .pipe(gulpif(/\.css$/, cssSlam()))
-          // .pipe(gulpif(/\.html$/, htmlMinifier()))
+
+          // If you want to optimize, minify, compile, or otherwise process
+          // any of your source code for production, you can do so here before
+          // merging your sources and dependencies together.
           .pipe(gulpif(/\.(png|gif|jpg|svg)$/, imagemin()))
-          .pipe(polymerProject.rejoinHtml());
 
-        // Okay, now let's do the same to your dependencies
+          // The `sourcesStreamSplitter` created above can be added here to
+          // pull any inline styles and scripts out of their HTML files and
+          // into seperate CSS and JS files in the build stream. Just be sure
+          // to rejoin those files with the `.rejoin()` method when you're done.
+          .pipe(sourcesStreamSplitter.split())
+
+          // Uncomment these lines to add a few more example optimizations to your source files.
+          // .pipe(gulpif(/\.js$/, uglify())) // Install gulp-uglify to use
+          // .pipe(gulpif(/\.css$/, cssSlam())) // Install css-slam to use
+          // .pipe(gulpif(/\.html$/, htmlMinifier())) // Install gulp-html-minify to use
+
+          // Remember, you need to rejoin any split inline code when you're done.
+          .pipe(sourcesStreamSplitter.rejoin());
+
+
+        // Similarly, you can get your dependencies seperately and perform
+        // any dependency-only optimizations here as well.
         let dependenciesStream = polymerProject.dependencies()
-          .pipe(polymerProject.splitHtml())
-          // .pipe(gulpif(/\.js$/, uglify()))
-          // .pipe(gulpif(/\.css$/, cssSlam()))
-          // .pipe(gulpif(/\.html$/, htmlMinifier()))
-          .pipe(polymerProject.rejoinHtml());
+          .pipe(dependenciesStreamSplitter.split())
+          // Add any dependency optimizations here.
+          .pipe(dependenciesStreamSplitter.rejoin());
 
-        // Okay, now let's merge them into a single build stream
+
+        // Okay, now let's merge your sources & dependencies together into a single build stream.
         let buildStream = mergeStream(sourcesStream, dependenciesStream)
           .once('data', () => {
             console.log('Analyzing build dependencies...');
